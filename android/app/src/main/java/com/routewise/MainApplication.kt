1c98a2ddf52a9ba7223f1efdc5e9efff2aec6c5e
Silver Walusimbi <walusimbisilver90@gmail.com>
2025-05-09 10:45:23 +0300

    Generating apk.

--- android/app/src/main/java/com/routewise/MainApplication.kt
+++ android/app/src/main/java/com/routewise/MainApplication.kt
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import com.swmansion.reanimated.ReanimatedJSIModulePackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -28,6 +29,8 @@ class MainApplication : Application(), ReactApplication {
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+
+        override fun getJSIModulePackage() = ReanimatedJSIModulePackage()
       }
 
   override val reactHost: ReactHost
@@ -37,7 +40,6 @@ class MainApplication : Application(), ReactApplication {
     super.onCreate()
     SoLoader.init(this, OpenSourceMergedSoMapping)
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      // If you opted-in for the New Architecture, we load the native entry point for this app.
       load()
     }
   }
